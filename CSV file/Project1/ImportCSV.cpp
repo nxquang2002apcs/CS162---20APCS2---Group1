687150a409b3ca8dc06cbcf4ff2b7624520c4f9e
nxquang2002apcs <nxquang20@apcs.com>
2021-04-05 00:10:57 +0700

    Update

--- CSV file/Project1/ImportCSV.cpp
+++ CSV file/Project1/ImportCSV.cpp
@@ -1,25 +1,28 @@
 #include "DataStructure.h"
 
-void inputCSV(string path, Student** ListStudent) {
+
+void inputCSV(string path, Student* ListStudent, int& size) {
+    
     ifstream in;
     in.open(path);
-    if (!in.is_open())
+    if (!in.is_open()) {
         cout << "Could not open file";
-    char line[1000];
-    in.getline(line, 1000);
-    int i = 1;
-    while (in) {
-        ListStudent[i - 1] = new Student;
-        ListStudent[i-1]->no = i;
-        ListStudent[i-1]->SID = strtok(line, ",");
-        ListStudent[i-1]->firstName = strtok(NULL, ",");
-        ListStudent[i-1]->lastName = strtok(NULL, ",");
-        ListStudent[i-1]->gender = strtok(NULL, ",");
-        ListStudent[i-1]->socialID = strtok(NULL, ",");
-        ListStudent[i-1]->DateOfBirth = strtok(NULL, ",");
-        in.getline(line, 1000);
-        i++;
+        return;
     }
-    in.close();
-
+    else {
+        int i = 0;
+        while (!in.eof()) {
+            ListStudent[i].no = i + 1;
+            size++;
+            getline(in, ListStudent[i].SID, ',');
+            getline(in, ListStudent[i].firstName, ',');
+            getline(in, ListStudent[i].lastName, ',');
+            getline(in, ListStudent[i].gender, ',');
+            getline(in, ListStudent[i].socialID, ',');
+            getline(in, ListStudent[i].DateOfBirth, '\n');
+            i++;
+        }
+        in.close();
+    }
+    size -= 1;
 };
