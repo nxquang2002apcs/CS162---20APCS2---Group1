42f1b9da972ee97bc0c201025360a4bb76b5ad97
legiakhanh27 <81347629+legiakhanh27@users.noreply.github.com>
2021-04-18 16:24:03 +0700

    Xóa hàm signup

--- SignIn_SignUp.cpp
+++ SignIn_SignUp.cpp
@@ -39,6 +39,7 @@ void Read_Name_Pass(ifstream& in, string& username, string& password)
 	}
 }
 
+
 // Function to log in
 // Function will run until logging in sucessfully
 void login(ifstream& in, int& role)
@@ -55,116 +56,6 @@ void login(ifstream& in, int& role)
 	cout << "Log in successfully!" << endl;
 }
 
-// Function to check if the username entered by student is valid
-bool checkStringofStu(string s)
-{
-	if (s.length() != 8)
-		return false;
-
-	for (int i = 0; i < (s.length()); ++i)
-		if ((int)s[i] < 48 || (int)s[i] > 57)
-			return false;
-
-	return true;
-}
-
-// Function to check if the username entered by staff is valid
-bool checkStringofStaff(string s)
-{
-	if (s.length() <= 6)
-		return false;
-
-	string compare = "admin@";
-
-	for (int i = 0; i < 6; ++i)
-		if (s[i] != compare[i])
-			return false;
-
-	return true;
-}
-
-// Function to register
-void signup(ifstream& in, ofstream& out, int& role)
-{
-	string username, password;
-	string path;
-	bool check;
-
-	cout << "1. Student" << endl;
-	cout << "2. Staff" << endl;
-
-	// Choose student or staff
-	int choice;
-	do
-	{
-		cout << "Enter your choice (1 or 2): ";
-		cin >> choice;
-	} while (choice != 1 && choice != 2);
-	
-	role = choice;
-	
-	cin.ignore();
-
-	// Read in username and password
-	do
-	{
-		in.close(); // Close file after checking
-
-		if (choice == 1)
-		{
-			// Read in the username with the required student ID number
-			do
-			{
-				cout << "Username(username is your Student ID): ";
-				getline(cin, username);
-
-				check = checkStringofStu(username);
-
-				if (!check)
-					cout << "Username is your Student ID!" << endl;
-			} while (!check);
-		}
-		else
-		{
-			// Read in the username with admin@ at beginning
-			do
-			{
-				cout << "Username(start with admin@): ";
-				getline(cin, username);
-
-				check = checkStringofStaff(username);
-
-				if (!check)
-					cout << "Username must be started by admin@" << endl;
-			} while (!check);
-
-		}
-
-		// Read in password
-		cout << "Password: ";
-		getline(cin, password);
-
-		// Check whether username is used
-		path = "data\\" + username + ".txt";
-		in.open(path);
-		if (in.is_open())
-			cout << "Username is already taken!" << endl;
-	} while (in.is_open());
-	
-	// Write username and password in username.txt
-	out.open(path);
-	if (!out.is_open())
-	{
-		cout << "Can not create file" << endl;
-		return;
-	}
-
-	out << username << endl;
-	out << password << endl;
-	out.close();
-	
-	cout << "Sign up successfully! << endl;
-}
 
 // Function to change password
 void changePassword(ifstream& in, ofstream& out)
@@ -180,7 +71,8 @@ void changePassword(ifstream& in, ofstream& out)
 	out.open(path);
 	if (!out.is_open())
 	{
-		cout << "Can not create file" << endl;
+		cout << "Can not open file" << endl;
+		cout << "Changing password failed!" << endl;
 		return;
 	}
 
@@ -188,4 +80,6 @@ void changePassword(ifstream& in, ofstream& out)
 	out << newpassword << endl;
 
 	out.close();
+	
+	cout << "Change password successfully." << endl;
 }
