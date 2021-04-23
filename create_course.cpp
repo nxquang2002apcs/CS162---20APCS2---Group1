e9ed67051d3c69b9a5d38dcaf2414807affbf3e7
banana-guy <81234624+banana-guy@users.noreply.github.com>
2021-04-23 19:49:38 +0700

    Thêm một hàm tạo liên kết.
    Hàm create_connection được thêm vào dùng để tạo liên kết giữa một Student_CourseScores của một môn với một Student.

--- create_course.cpp
+++ create_course.cpp
@@ -180,3 +180,44 @@ void connect_course_scores_to_student ( )
 		pCur_semester_courses = pCur_semester_courses -> pNext;
 	}
 }
+
+void create_connection ( CourseDetail* &course, string StudentID, string className )	//	Hàm tạo liên kết một Student_CourseScores của một môn với một Student
+{
+	Class* pCur_class = CurrentYear -> HeadClass;	// Tìm lớp của sinh viên này đang học
+	while ( pCur_class != nullptr && pCur_class -> className != className )	// bằng cách so sánh className
+		pCur_class = pCur_class -> pNext;
+
+	if ( pCur_class != nullptr )	// Nếu tìm thấy lớp
+	{
+		Student* pCur_student = pCur_class -> HeadStudent;	// thì tìm sinh viên này trong lớp mới tìm được
+		while ( pCur_student != nullptr && pCur_student -> SID != StudentID )	// bằng cách so sánh ID sinh viên
+			pCur_student = pCur_student -> pNext;
+
+		if ( pCur_student != nullptr )	// Nếu tìm thấy sinh viên
+		{
+			CourseForEachStudent* pCur_enrolled_course = pCur_student -> Head_of_enrolled_course;	// thì tìm khóa học cần tạo liên kết trong danh sách khóa học sinh viên này đã đăng kí
+			while ( pCur_enrolled_course != nullptr && pCur_enrolled_course -> detail.courseID != course -> courseID )	// bằng cách so sánh ID khóa học
+				pCur_enrolled_course = pCur_enrolled_course -> pNext;
+
+			if ( pCur_enrolled_course != nullptr )	// Nếu tìm thấy
+			{
+				cout << "found" << endl;
+				Student_CourseScores* pCur_Student_CourseScores = course -> HeadStudent;	// thì tìm bảng điểm cần liên kết trong danh sách điểm của khóa học
+				while ( pCur_Student_CourseScores != nullptr && pCur_Student_CourseScores -> SID != pCur_student -> SID )	// bằng cách so sánh ID sinh viên
+					pCur_Student_CourseScores = pCur_Student_CourseScores -> pNext;
+
+				if ( pCur_Student_CourseScores != nullptr )	// Nếu tìm thấy
+				{
+					pCur_Student_CourseScores -> point_to_an_enrolled_course_of_a_student_in_a_class = pCur_enrolled_course;	// thì tạo liên kết
+
+					// --- Sao chép từ bảng điểm qua ----
+					pCur_enrolled_course -> midterm 	=	 pCur_Student_CourseScores -> midterm;
+					pCur_enrolled_course -> final		= 	 pCur_Student_CourseScores -> final;
+					pCur_enrolled_course -> otherMark	= 	 pCur_Student_CourseScores -> otherMark;
+					pCur_enrolled_course -> total		= 	 pCur_Student_CourseScores -> total;
+					// ----------------------------------
+				}
+			}
+		}
+	}
+}
