924b87a3f88f1088c6629b4645606d601ac7f38d
HuyDuongHoang <huyhoangduong326@gmail.com>
2021-04-27 20:36:56 +0700

    bonus nét đôi

--- draw_rec.cpp
+++ draw_rec.cpp
@@ -27,4 +27,29 @@ void DrawRectangleSingle(int x, int y, int width, int height){
 
 }
 
+//vẽ nét đôi
+void DrawRectangleDouble(int x, int y, int width, int height){
+    gotoXY(x, y);
+    cout << char(201);
+    for (int i = 0; i < width; ++i) {
+        cout << char(205);
+    }
+    cout << char(187);
 
+    for (int i = 1; i <= height; ++i) {
+        gotoXY(x, y + i);
+        cout << char(186);
+        for (int j = 0; j < width; ++j) {
+            cout << char(32);
+        }
+        cout << char(186);
+    }
+
+    gotoXY(x, y + height);
+    cout << char(200);
+    for (int i = 0; i < width; ++i) {
+        cout << char(205);
+    }
+    cout << char(188);
+
+}
