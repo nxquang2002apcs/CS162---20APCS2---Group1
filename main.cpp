24227dc9467d2f96ac23dc3254957dcdf63be0ec
nxquang2002apcs <nxquang20@apcs.com>
2021-04-28 23:31:16 +0700

    Đưa vào vòng lặp để làm log out

--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@ int main() {
 	ifstream in;
 	int role = 1;
 	string studentID;
-	if (login(in, role, studentID)) {
-		readAll();
-		if (role == 1)
-			StudentInterface(studentID);
-		else StaffInterface();
-		delete_everything();
+	bool logSuccess = false;
+	readAll();
+	while (true)   {
+		if (login(in, role, studentID)) {
+			if (role == 1)
+				StudentInterface(studentID);
+			else StaffInterface();
+		}
+		else break;  
 	}
+	delete_everything();
+
 	in.close();
 
 	
